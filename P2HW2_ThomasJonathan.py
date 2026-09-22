0035b80e4e210aca39ba5a76eeaab13aebd40983
thomasj4837 <thomasj4837@student.faytechcc.edu>
2026-09-22 18:57:51 +0000

    P2HW2_Finished

--- P2HW2_ThomasJonathan.py
+++ P2HW2_ThomasJonathan.py
@@ -12,7 +12,7 @@ grade5 = float(input("Enter grade #5: "))
 grade6 = float(input("Enter grade #6: "))
 
 # Put them all into a new list
-grade_list = [grade1, grade2, grade3, grade4, grade5, grade6]
+grade_list = grade1, grade2, grade3, grade4, grade5, grade6
 
 # Do some calculations -- minimum, maximum, and average
 min_grade = min(grade_list)
@@ -22,5 +22,9 @@ count     = len(grade_list)
 
 #calc average (total / count)
 average = total / count
-print(average)
-# all thats left is print results
+print(f"Grades: {grade_list}")
+print(f"{"lowest:":<25} {min_grade}")
+print(f"{"Highest:":<25} {max_grade}")
+print(f"{"Sum of Grades":<25} {total}")
+print(f"{"Average":<25} {average:.2f}")
+
